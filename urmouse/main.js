1a8e6e24299c27f035e1a45ea19f9e38bddb62fe
shawn <fu@2908.artic.edu>
2017-12-06 19:08:28 -0600

    lame graphic w/ lame info

--- urmouse/main.js
+++ urmouse/main.js
@@ -2,24 +2,19 @@
 //mouse move
 var m = document.createElement('div');
 document.body.appendChild(m);
-
-//nick:
-// "give the element-object a CSS position style of "absolute"
-// this will then allow us to set it's left/top coordinates later on"
 m.style.position = "fixed";
 
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+//以下修改点击鼠标以后出现的信息内容 ``符号是可以让内容竖排下来
+var codeString = `let mousePos = document.createElement('div');
+mousePos.textContent = e.x + " " + e.y;
 
-var codeString = `  var div = document.createElement('div');
-div.textContent = codeString;
-div.style.position = "absolute";
-document.body.appendChild( div );
+let appV = document.createElement('div');
+appV.textContent = navigator.appVersion;
+
+let appCodeName = document.createElement('div');
+appCodeName.textContent = navigator.appCodeName;
 `;
-
-//try to make the codeString only appear when i click and will disappear
-// var div = document.createElement('div');
-// div.textContent = codeString;
-// div.style.position = "fixed";
-// document.body.appendChild( div );
 //
 var pre;
 function MouseDown(e){
@@ -27,23 +22,24 @@ function MouseDown(e){
   pre.textContent = codeString;
   // use the e.x && e.y to dynamically place the pre tag
   // wherever the mouse happens to be when it's clicked
+  //style of the mouseclick
   pre.style.position = "fixed";
-  pre.style.left = e.x+10+"px";
-  pre.style.top = e.y+10+"px";
+  pre.style.left = e.x+20+"px";
+  pre.style.top = e.y+20+"px";
+	pre.style.fontSize = 12+"px";
+	pre.style.lineHeight = 25+"px";
+	pre.style.color = "white";
   pre.style.backgroundColor = "red";
   document.body.appendChild(pre);
 }
 
 window.addEventListener('mousedown',MouseDown);
-
+//release the mouse and the info disappear
 function MouseUp(e){
   document.body.removeChild(pre);
 }
 
 window.addEventListener('mouseup',MouseUp);
-// setTimeout (function(){
-//   onMouseClick(e);
-// },1000);
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
@@ -52,44 +48,89 @@ function updateMouseXY(e){
 
 
 //以下是可以添加的信息内容
-//let vs var
-
  let mousePos = document.createElement('div');
- mousePos.textContent = e.x + " " + e.y;
+ mousePos.textContent = `Hello there, we've noticed that: your mouse is moving at `+ e.x + "(x)" + " " + e.y + "(y)" + " in this window";
 
  let appV = document.createElement('div');
- appV.textContent = navigator.appVersion;
+ appV.textContent = "your browser version is " + navigator.appVersion;
+
+
+ let osCPU = document.createElement('div');
+ osCPU.textContent = "and you have " + navigator.hardwareConcurrency + " CPU running in your computer";
+
+ let cpuInfo = document.createElement('div');
+ cpuInfo.textContent = "Information about the OS and CPU: " + navigator.oscpu;
 
  let appCodeName = document.createElement('div');
- appCodeName.textContent = navigator.appCodeName;
+ appCodeName.textContent = "and you are using the " + platform.name + " browser!!";
+ // let cpuClass = document.createElement('div');
+ // cpuClass.textContent = "The class of CPU: " + navigator.cpuClass;
 
- let appPlugins = document.createElement('div');
- appPlugins.textContent = navigator.plugins;
+ let osPlatform = document.createElement('div');
+ osPlatform.textContent = "your system platform is " + navigator.platform;
+
+
+//navigator.plugins
+
+ // let appPlugins = document.createElement('div');
+ // appPlugins.textContent = navigator.plugins;
 
 let timeZone = document.createElement('div');
-timeZone.textContent = Intl.DateTimeFormat()
+timeZone.textContent = "btw, you are in this time zone " + Intl.DateTimeFormat()
 .resolvedOptions()
-.timeZone;
-//oscpu userAgent language Screen resolution
+.timeZone + " ,aren't you?";
+
+// ~~~~~~~~~~~~~~~~~~~
+// let plugins = [
+// 	{name:'first'},
+//   {name:'second'},
+//   {name:'third'},
+//   {name:'fourth'},
+//   {name:'fifth'}
+// ];
+//
+// let pluginsArray = document.createElement('div');
+//
+// for(var i=0;i<plugins.length;i++){
+// 	pluginsArray.textContent += plugins[i].name+', ';
+// }
+// pluginsArray.textContent = navigator.plugins;
+
+//m.appendChild(pluginsArray);
+// ~~~~~~~~~~~~~~~~~~~~~
+//userAgent language Screenresolution
 //Timezone Fonts HTTP ACCEPT Supercookies HTML5 Canvas...
 
 //let scrollElm = document.scrollingElement;
 //scrollElm.scrollTop = 0;
 //document.cookie;
-
+/*~~~~*/
+//每加一项data 都需要在这里叫一下 关联
   m.appendChild(mousePos);
   m.appendChild(appV);
-  m.appendChild(appCodeName);
-  m.appendChild(appPlugins);
+
+  // m.appendChild(appPlugins);
+
+  m.appendChild(osCPU);
+  m.appendChild(cpuInfo);
+  //m.appendChild(cpuClass);
+  m.appendChild(osPlatform);
+	m.appendChild(appCodeName);
   m.appendChild(timeZone);
+/*~~~~*/
+  //style of the mousemove
 	m.style.left = e.x+20+"px";
-  m.style.top = e.y+50+"px";
+  m.style.top = e.y+20+"px";
+  m.style.color = "white";
+  m.style.backgroundColor = "black";
+	m.style.lineHeight = 25+"px";
+	m.style.padding = 15+"px";
+	// m.style.marginBottom = "50px";
+	// m.style.borderBottom = "50px";
+	// m.style.marginLeft = "10px";
+	m.style.fontSize = 12+"px";
+
 }
 
-
-
-
-
-
-
+console.log(platform);
 window.addEventListener('mousemove', updateMouseXY);

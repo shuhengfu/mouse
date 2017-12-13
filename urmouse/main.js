3b4f80c14fac38adc8228c3ac24a819bf4b4f9e4
shawn <fu@2908.artic.edu>
2017-12-13 00:17:41 -0600

    organized

--- urmouse/main.js
+++ urmouse/main.js
@@ -6,14 +6,76 @@ m.style.position = "fixed";
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 //以下修改点击鼠标以后出现的信息内容 ``符号是可以让内容竖排下来
-var codeString = `let mousePos = document.createElement('div');
-mousePos.textContent = e.x + " " + e.y;
+var codeString = `
+var m = document.createElement('div');
+document.body.appendChild(m);
+m.style.position = "fixed";
 
-let appV = document.createElement('div');
-appV.textContent = navigator.appVersion;
+var pre;
+function MouseDown(e){
+	pre = document.createElement('pre');
+  pre.textContent = codeString;
+	pre.style.position = "fixed";
+	pre.style.left = e.x+20+"px";
+	pre.style.top = e.y+20+"px";
+	pre.style.fontSize = 12+"px";
+	pre.style.lineHeight = 25+"px";
+	pre.style.color = "white";
+	pre.style.backgroundColor = "red";
+	document.body.appendChild(pre);
+}
+window.addEventListener('mousedown',MouseDown);
 
-let appCodeName = document.createElement('div');
-appCodeName.textContent = navigator.appCodeName;
+function updateMouseXY(e){
+	m.innerHTML = "";
+
+ let mousePos = document.createElement('div');
+ mousePos.textContent = e.x + "(x)" + " " + e.y + "(y)";
+ let screenRes = document.createElement('div');
+ screenRes.textContent = screen.width + "(px) x "+ screen.height + "(px)";
+ let appCodeName = document.createElement('div');
+ appCodeName.textContent = platform.name;
+ let hostName = document.createElement('div');
+ hostName.textContent = location.hostname;
+ let lastPage = document.createElement('div');
+ lastPage.textContent = document.referrer;"
+ let appV = document.createElement('div');
+ appV.textContent = navigator.appVersion;
+ let cpuInfo = document.createElement('div');
+ cpuInfo.textContent = navigator.oscpu;
+ let osPlatform = document.createElement('div');
+ osPlatform.textContent = navigator.platform;
+ let osCPU = document.createElement('div');
+ osCPU.textContent = navigator.hardwareConcurrency;
+ let timeZone = document.createElement('div');
+ timeZone.textContent =Intl.DateTimeFormat()
+ .resolvedOptions().timeZone + " ,aren't you?";
+ let cookie = document.createElement('div');
+ cookie.textContent =document.cookie;
+
+ m.appendChild(mousePos);
+ m.appendChild(screenRes);
+ m.appendChild(appCodeName);
+ m.appendChild(hostName);
+ m.appendChild(lastPage);
+ m.appendChild(appV);
+ m.appendChild(cpuInfo);
+ m.appendChild(osPlatform);
+ m.appendChild(osCPU);
+ m.appendChild(timeZone);
+ m.appendChild(cookie);
+
+ m.style.left = e.x+20+"px";
+ m.style.top = e.y+20+"px";
+ m.style.color = "white";
+ m.style.backgroundColor = "black";
+ m.style.lineHeight = 25+"px";
+ m.style.padding = 15+"px";
+ m.style.fontSize = 12+"px";
+
+}
+
+window.addEventListener('mousemove', updateMouseXY);
 `;
 //
 var pre;
@@ -80,46 +142,22 @@ greet.textContent = "* Hello there! It's good to know that the website you are v
  let osCPU = document.createElement('div');
  osCPU.textContent = "you have " + navigator.hardwareConcurrency + " CPUs running in your computer. Isn't that exciting?!";
 
-let timeZone = document.createElement('div');
-timeZone.textContent = "and you are in the time zone of " + Intl.DateTimeFormat()
-.resolvedOptions()
-.timeZone + " ,aren't you?";
+ let timeZone = document.createElement('div');
+ timeZone.textContent = "and you are in the time zone of " + Intl.DateTimeFormat()
+ .resolvedOptions()
+ .timeZone + " ,aren't you?";
 //hope you enjoy the daylight/nighttime?
 //if condition with realtime of certain timezone...nm
 
-let cookie = document.createElement('div');
-cookie.textContent = "btw, here is the cookie on this page, in case you have any interest "+document.cookie;
+ let cookie = document.createElement('div');
+ cookie.textContent = "btw, here is the cookie on this page, in case you have any interest "+document.cookie;
 
 let bye = document.createElement('div');
 bye.textContent = "* and thank you for sharing without knowing (: *";
 
-// let cookie = document.createElement('div');
-// cookie.textContent = "cookie " + getCookie();
-//
-// function getCookie(name){
-//     /* 获取浏览器所有cookie将其拆分成数组 */
-//     var arr=document.cookie.split('; ');
-//
-//     for(var i=0;i<arr.length;i++)    {
-//         /* 将cookie名称和值拆分进行判断 */
-//         var arr2=arr[i].split('=');
-//         if(arr2[0]==name){
-//             return arr2[1];
-//         }
-//     }
-//     return '';
-// }
+//i got the cookie info but donno what it really stands for with
+//bunch of letters and numbers...and how to convert it into human language
 
-//the language you are using is .. navigator.language
-
-// let pathName = document.createElement('div');
-// pathName.textContent = "and you are searching " + location.pathname;
-//userAgent language Screenresolution
-//Timezone Fonts HTTP ACCEPT Supercookies HTML5 Canvas...
-
-//let scrollElm = document.scrollingElement;
-//scrollElm.scrollTop = 0;
-//document.cookie;
 /*~~~~*/
 //每加一项data 都需要在这里叫一下 关联
   m.appendChild(greet);
@@ -129,15 +167,13 @@ bye.textContent = "* and thank you for sharing without knowing (: *";
 	m.appendChild(hostName);
 	m.appendChild(lastPage);
  	m.appendChild(appV);
-	// m.appendChild(language);
 	m.appendChild(cpuInfo);
 	m.appendChild(osPlatform);
   m.appendChild(osCPU);
   m.appendChild(timeZone);
   m.appendChild(cookie);
 	m.appendChild(bye);
-//i got the cookie info but donno what it really stands for with
-//bunch of letters and numbers...and how to convert it into human language
+	// m.appendChild(language);
 /*~~~~*/
 
   //style of the mousemove
@@ -151,5 +187,4 @@ bye.textContent = "* and thank you for sharing without knowing (: *";
 
 }
 
-//console.log(platform);
 window.addEventListener('mousemove', updateMouseXY);
